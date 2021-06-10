bead024a33fe741e7ac19acf7ecfafacb993e6c9
da-head0 <andyourturntome@gmail.com>
2021-06-10 18:53:24 +0900

    auth 삭제

--- main.py
+++ main.py
@@ -9,20 +9,14 @@ from function.recommendation import compare_taste
 import uvicorn
 from starlette.responses import RedirectResponse, Response
 from starlette.requests import Request
-from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordRequestForm
-from fastapi_login import LoginManager
-from fastapi_login.exceptions import InvalidCredentialsException
 import os
 from datetime import timedelta
 from pydantic import BaseModel
 
 
-
 app = FastAPI()
 
 security = HTTPBasic()
-SECRET = '3066ba5df3bf0d588ab84f69304cf991277a0847947d954c'
-manager = LoginManager(SECRET, token_url='/auth/token', use_cookie=True)
 
 origins = ["*"]
 app.add_middleware(
@@ -34,13 +28,6 @@ app.add_middleware(
 )
 
 templates = Jinja2Templates(directory="templates")
-
-class FoodItem(BaseModel):
-    title: str
-
-class UserCreate(BaseModel):
-    cat_name: str
-    cat_age: str
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -136,60 +123,3 @@ async def bytitle(request: Request,foodtitle: str = None, stars : int=None):
     if stars:
         rating_col.update_one({"cat_id":cat_id, "title":foodtitle}, {"$set" : {"rating":stars}})
     return templates.TemplateResponse("rated.html", {"request": request, "data":ratedlist})
-
-
-@manager.user_loader
-def load_cat(catname:str, catage:str):
-    data = "어떤 고양이를 키우시나요?"
-    catindex = countcatnum()
-    existcat = cat_col.find_one({"cat_name":catname}) 
-    #return {"고양이 이름": credentials.username, "고양이 나이": credentials.password}
-    if not existcat:
-        cat_col.insert_one({"cat_id":catindex, "cat_name":catname, "cat_age":catage})
-        message = "고양이 정보가 추가되었습니다"
-        return catname, catage 
-        #templates.TemplateResponse("user.html", {"request": request, "data":data, "message" : message})
-    
-    else: # 고양이를 중복 입력할 경우
-        cat_col.update({"cat_name":catname}, {"$set" : {"cat_id":catindex-1, "cat_age":catage}})
-    #return templates.TemplateResponse("user.html", {"request": request, "data":data})
-
-
-@app.get('/auth')
-def auth(response: Response, user=Depends(manager)):
-    token = manager.create_access_token(
-        data=dict(sub=user.cat_name)
-    )
-    manager.set_cookie(response, token)
-    return response
-
-@app.post("/auth/register")
-def register(user: UserCreate):
-    existlist = []
-    existcat = cat_col.find({'cat_name':user.cat_name})
-    catindex = countcatnum()
-    for cat in existcat:
-        existlist.append(cat)
-    if user.cat_name in existlist:
-        raise HTTPException(status_code=400, detail="A user with this email already exists")
-    else:
-        cat_col.insert_one({"cat_id":catindex, "cat_name":user.cat_name, "cat_age":user.cat_age})
-        return {"detail": "Successfull registered"}
-
-# the python-multipart package is required to use the OAuth2PasswordRequestForm
-@app.post('/auth/token')
-def login(data: OAuth2PasswordRequestForm = Depends()):
-    cat_name = data.catname
-    cat_age = data.catage
-
-    user = load_cat(cat_name, cat_age)  # we are using the same function to retrieve the user
-    if not user:
-        raise InvalidCredentialsException  # you can also use your own HTTPException
-    elif cat_age != user['password']:
-        raise InvalidCredentialsException
-    
-    access_token = manager.create_access_token(
-        data=dict(sub=cat_name), expires=timedelta(hours=12)
-    )
-    return {'access_token': access_token, 'token_type': 'bearer'}
-
